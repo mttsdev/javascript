e35a97d8e8001567c6debde57c37753fe24a4988
MatheusSantos <santosdasilvamatheus39@gmail.com>
2025-08-02 11:46:47 -0300

    ex011

---
+++ aula012/ex011.js
@@ -0,0 +1,13 @@
+var idade = 19
+var num = 21
+if (idade <=17) {
+    console.log('Você não vota')
+} else {
+    console.log('Você vota')
+    console.log('Qual o número do candidato que deseja votar?')
+    if(num != 25) {
+        console.log('Este candidato não existe')
+    } else {
+        console.log('Você votou no candidato 25')
+}
+}
